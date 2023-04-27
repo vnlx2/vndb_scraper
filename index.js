22fa0b6a4e0dc7b57140ee8f297a343cd5a37f7f
Taufiq Pohan <m.taufiq30s@hotmail.com>
2023-04-28 00:46:10 +0800

    index: Implement cron and progress bar
    Redesign CLI interface with progress bar, and create cron so
    scraper will be run everyday at 12.00 AM (UTC+7).
    
    Signed-off-by: Taufiq Pohan <m.taufiq30s@hotmail.com>

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import { config } from "dotenv";
 import model from './VNDBModel.js';
 import fs from 'fs';
 import logger from './logger.js';
+import cliProgress from 'cli-progress';
+import { schedule } from 'node-cron';
 
 config();
 
@@ -15,6 +17,108 @@ async function get_vn_by_code(codes)
 	} catch (err) {
 		console.error(err);
 	}
+}
+
+async function insert_to_db(results, currentCode)
+{
+	const documents = [];
+
+	// Add result to model
+	for(const result of results) {
+		logger.info(`Processing ${result.id} and current code ${currentCode}`);
+		while(parseInt(currentCode) < parseInt(result.id)) {
+			logger.error(`VN with code ${currentCode} not found`);
+			currentCode++;
+		}
+		const body = {
+			code: result.id,
+			title: result.title,
+			aliases: (result.aliases != null) ? result.aliases.split(/\r?\n/) : null,
+			length: result.length,
+			rating: result.rating,
+			description: (!result.description) ? '-' : result.description,
+			image: result.image
+		};
+		documents.push(body);
+		currentCode++;
+	}
+
+	// Store vn data and return last code and error counter
+	try {
+		const response = model;
+		await response.insertMany(documents, {ordered : false });
+	} catch (err) {
+		throw err;
+	}
+	return currentCode;
+}
+
+function save_last_id_and_error_counter(id, error_count=0)
+{
+	const jsonVal = {
+		last_vn_id: id,
+		error_count: error_count
+	};
+	fs.writeFileSync('vn-stats.json', JSON.stringify(jsonVal)+"\n");
+	return true;
+}
+
+function sleep(ms)
+{
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms);
+	});
+}
+
+async function scrape_vn_and_save_to_db(lastCode, remainsVN, batch, errorCount)
+{
+	const progressBar = new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic);
+	progressBar.start(batch, 0);
+
+	try {
+		let start = performance.now();
+		while (batch > 0) {
+			const length = (remainsVN >= 10) ? 10 : remainsVN;
+			// Generate array of codes based from last code to last code + length
+			const codes = [...Array(length).keys()].map(code => code + lastCode);
+	
+			// Scrap from vndb
+			const response = await get_vn_by_code(codes);
+			if (!response) {
+				console.log("Internal error");
+				save_last_id_and_error_counter(lastCode, errorCount);
+				progressBar.stop();
+				break;
+			}
+			logger.info(`Total of success vn scraped: ${response.items.length}`);
+			
+			// Store to db
+			lastCode = await insert_to_db(response.items, lastCode);
+			save_last_id_and_error_counter(lastCode);
+			batch--;
+			remainsVN -= length;
+			progressBar.increment();
+			await sleep(1000);
+		}
+		return performance.now() - start;
+	} catch (err) {
+		progressBar.stop();
+		throw err;
+	}
+}
+
+function get_last_id_and_error_counter()
+{
+	if (!fs.existsSync('./vn-stats.json'))
+		return {lastVNId: 0, errorCount: 0};
+
+	const jsonVal = fs.readFileSync('./vn-stats.json');
+	let ret = JSON.parse(jsonVal);
+	if ((!("last_vn_id" in ret) || isNaN(ret.last_vn_id)) &&
+		(!("error_count" in ret) || isNaN(ret.error_count)))
+		return {lastVNId: 1, errorCount: 0};
+
+	return {lastVNId: ret.last_vn_id, errorCount: ret.error_count};
 }
 
 async function get_number_of_vndb_vns()
@@ -31,156 +135,54 @@ async function get_number_of_our_vns()
 	return await model.countDocuments();
 }
 
-async function insert_to_db(results, currentCode, errorCounter)
-{
-	const documents = [];
-	// Set expectation of last code after scraping
-	let lastCode = currentCode + 10;
-
-	// Add result to model
-	for(const result of results) {
-		logger.info(`Processing ${result.id} and current code ${currentCode}`);
-		while(parseInt(currentCode) < parseInt(result.id)) {
-			console.log(`Failed to scrape VN ${currentCode}`);
-			logger.error(`Failed to scrape ${currentCode}`);
-			currentCode++;
-			errorCounter++;
-		}
-		const body = {
-			code: result.id,
-			title: result.title,
-			aliases: (result.aliases != null) ? result.aliases.split(/\r?\n/) : null,
-			length: result.length,
-			rating: result.rating,
-			description: (!result.description) ? '-' : result.description,
-			image: result.image
-		};
-		documents.push(body);
-		currentCode++;
-	}
-	/** 
-	 * When last code not reach with last vn codes that want to scrap, 
-	 * return error and add 1 to current code
-	*/  
-	if(lastCode != currentCode) {
-		currentCode++;
-		console.log(`Failed to scrape VN ${lastCode}`);
-		logger.error(`Failed to scrape ${currentCode}`);
-		errorCounter++;
-	}
-
-	// Store vn data and return last code and error counter
-	try {
-		const response = model;
-		await response.insertMany(documents, {ordered : false });
-	} catch (err) {
-		console.error(err);
-	}
-	logger.info(`Last Code after scrap ${lastCode} and error counter ${errorCounter}`);
-	return {lastCode: currentCode-1, errorCounter: errorCounter};
-}
-
-async function scrape_vn_and_save_to_db(lastCode, remainsVN, errorCounter)
-{
-	// Generate vndb code sequences
-	// Get length of array of code
-	const length = (remainsVN >= 10) ? 10 : remainsVN;
-
-	console.log(`Scraping VN ${lastCode} - ${lastCode + length - 1}...`);
-	logger.info(`Scraping VN ${lastCode} - ${lastCode + length - 1}...`);
-
-	// Generate array of codes based from last code to last code + length
-	const codes = [...Array(length).keys()].map(code => code + lastCode);
-
-	// Get VN data
-	const response = await get_vn_by_code(codes);
-	if (!response) {
-		console.log("Internal error");
-		return {
-			status: false,
-			lastCode: lastCode,
-			errorCounter: errorCounter,
-		};
-	}
-	logger.info(`Total of success vn scrap: ${response.items.length}`);
-
-	// Store to db
-	const result = await insert_to_db(
-		response.items, lastCode,  errorCounter
-		);
-	
-	return {
-		status: true,
-		lastCode: result.lastCode,
-		errorCounter: result.errorCounter,
-	};
-}
-
-function save_last_id_and_error_counter(id, error_count=0)
-{
-	const jsonVal = {
-		last_vn_id: id,
-		error_count: error_count
-	};
-	fs.writeFileSync('vn-stats.json', JSON.stringify(jsonVal)+"\n");
-	return true;
-}
-
-function get_last_id_and_error_counter()
-{
-	if (!fs.existsSync('./vn-stats.json'))
-		return {lastVNId: 0, errorCount: 0};
-
-	const jsonVal = fs.readFileSync('./vn-stats.json');
-	let ret = JSON.parse(jsonVal);
-	if ((!("last_vn_id" in ret) || isNaN(ret.last_vn_id)) &&
-		(!("error_count" in ret) || isNaN(ret.error_count)))
-		return {lastVNId: 0, errorCount: 0};
-
-	return {lastVNId: ret.last_vn_id, errorCount: ret.error_count};
-}
-
-function sleep(ms)
-{
-	return new Promise((resolve) => {
-		setTimeout(resolve, ms);
-	});
-}
-
 async function start_scrape()
 {
+	console.log('\nStart Scraping');
 	// Get Last VN Code and Error Counter
 	const lastIdAndErrorCounter = get_last_id_and_error_counter();
-	let i = lastIdAndErrorCounter.lastVNId + 1;
+	let lastCode = lastIdAndErrorCounter.lastVNId;
 	let errorCount = lastIdAndErrorCounter.errorCount;
-	let nr_vns_vndb = await get_number_of_vndb_vns();
 
-	while (true) {
-		let nr_vns_ours = await get_number_of_our_vns();
-		let remains = nr_vns_vndb - nr_vns_ours;
-
-		logger.info(`VN Stat : { 
-			our_vn_count: ${nr_vns_ours}, vndb_count: ${nr_vns_vndb}, 
-			remains: ${remains} }`);
-
-		if (nr_vns_vndb == nr_vns_ours)
-			break;
-		let ret = await scrape_vn_and_save_to_db(i, remains, 
-			errorCount);
-
-		console.log(`Successfully scraped VN ${i} - ${ret.lastCode}`);
-		logger.info(`Successfully scraped VN ${i} - ${ret.lastCode}`);
-		save_last_id_and_error_counter(ret.lastCode, ret.errorCounter);
-		i = ret.lastCode+1;
-		errorCount = ret.errorCounter;
-		await sleep(1000);
+	// Count remains vn that not inserted to database
+	const nr_vns_vndb = await get_number_of_vndb_vns();
+	const nr_our_vns = await get_number_of_our_vns();
+	let remains = nr_vns_vndb - nr_our_vns;
+	
+	if (nr_our_vns === 0) {
+		lastCode = 1;
+		errorCount = 0;
 	}
-	process.exit();
+
+	console.info(`There are ${remains} vn that not added.`);
+	logger.info(`VN Stat : { 
+		our_vn_count: ${nr_our_vns}, vndb_count: ${nr_vns_vndb}, 
+		remains: ${remains} }`);
+	
+	// Calculate batch
+	const batches = Math.ceil(remains / 10);
+
+	// Execution
+	try {
+		const timer = await scrape_vn_and_save_to_db(lastCode, remains, batches,
+			errorCount);	
+		console.log('\n\nScraping VN Success');
+
+		const current_nr_our_vns = await get_number_of_our_vns();
+		console.log(`Success : ${current_nr_our_vns - nr_our_vns}`);
+		console.log(`Failed  : ${nr_vns_vndb - current_nr_our_vns}`);
+		console.log(`Timer   : ${((timer - (1000 * (batches - 1))) / 1000).toFixed(2)} seconds`);
+		logger.info(`Success : ${current_nr_our_vns - nr_our_vns} ; Failed : ${nr_vns_vndb - current_nr_our_vns}`);
+	} catch (err) {
+		console.error('An error was occured.');
+		console.error(err);
+		logger.error(err);
+	}
 }
 
 function main()
 {
 	console.log(`VNDB Scraper ${process.env.VERSION}`);
+	console.log('Visual Novel Lovers - 2023\n');
 	mongoose.connect(process.env.MONGODB_URI, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true
@@ -189,8 +191,12 @@ function main()
 	mongoose.connection
 		.on('error', (error) => console.error(error))
 		.once('open', async function () {
-			console.log('Database Connected');
-			await start_scrape();
+			console.log('[Info] : Database Connected');
+			logger.info('Database Connected');
+			schedule('0 0 * * *', async () => await start_scrape(), {
+				scheduled: true,
+				timezone: "Asia/Jakarta"
+			});
 		});
 }
 
